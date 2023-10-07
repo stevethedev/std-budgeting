652d82d578a6893956624ccbdab888959ea2425d
Steven Jimenez <stevenmjimenez@gmail.com>
2023-10-07 13:37:24 -0400

    Remove possible security bug in the currency component

--- packages/std-budgeting-client/src/component/currency/currency.tsx
+++ packages/std-budgeting-client/src/component/currency/currency.tsx
@@ -18,25 +18,12 @@ export const Currency: FC<Props> = ({
   isBlock = false,
   ...props
 }) => {
-  const formatter = new Intl.NumberFormat(locales, {
+  const { prefix, value, suffix } = formatCurrency(children, locales, {
     style: "currency",
     currency: "USD",
 
     ...options,
   });
-
-  const isNegative = children < 0;
-  const text = formatter.format(Math.abs(children));
-
-  const {
-    prefix,
-    value = "",
-    suffix = "",
-  } = text.match(/^(?<prefix>\D+)?\s*(?<value>[\d.,]+)\s*(?<suffix>\D+)?$/)
-    ?.groups ?? {};
-
-  const dataPrefix = isNegative ? `${prefix} (` : `${prefix} `;
-  const dataSuffix = isNegative ? `) ${suffix}` : ` ${suffix}`;
 
   const classes = getClassName([
     className,
@@ -49,11 +36,84 @@ export const Currency: FC<Props> = ({
   return (
     <span
       className={classes}
-      data-prefix={dataPrefix}
-      data-suffix={dataSuffix}
+      data-prefix={prefix}
+      data-suffix={suffix}
       {...props}
     >
       {children === 0 ? "-" : value}
     </span>
   );
+};
+
+interface FormattedCurrencyProps {
+  prefix: string;
+  value: string;
+  suffix: string;
+}
+
+const formatCurrency = (
+  value: number,
+  locales: string | string[],
+  options: Intl.NumberFormatOptions,
+): FormattedCurrencyProps => {
+  const isZero = value === 0;
+  const isNegative = value < 0;
+
+  const formatter = new Intl.NumberFormat(locales, options);
+  const parts = formatter.formatToParts(Math.abs(value));
+
+  const prefixParts: string[] = [];
+  const suffixParts: string[] = [];
+  const valueParts: string[] = [];
+
+  for (const part of parts) {
+    switch (part.type) {
+      case "nan":
+      case "infinity":
+        throw new Error("Cannot format infinity or NaN as currency.");
+
+      case "integer":
+      case "group":
+      case "decimal":
+      case "fraction":
+        valueParts.push(part.value);
+        break;
+
+      case "plusSign":
+      case "minusSign":
+        // discard
+        break;
+
+      case "literal":
+      case "percent":
+      case "percentSign":
+      case "currency":
+      case "code":
+      case "symbol":
+      case "name":
+      case "compact":
+      case "exponentInteger":
+      case "exponentMinusSign":
+      case "exponentSeparator":
+      case "unit":
+      case "unknown":
+        if (valueParts.length === 0) {
+          prefixParts.push(part.value);
+          break;
+        }
+        suffixParts.push(part.value);
+        break;
+      default:
+        throw new Error(`Unknown part type: ${part.type}`);
+    }
+  }
+
+  prefixParts.push(isNegative ? " (" : "  ");
+  suffixParts.unshift(isNegative ? ") " : "  ");
+
+  return {
+    prefix: prefixParts.join(""),
+    value: isZero ? "-" : valueParts.join(""),
+    suffix: suffixParts.join(""),
+  };
 };
